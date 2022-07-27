772f9201888d0fa480af1d4f253a083fd7bb1456
Zeuss0910 <107348151+Zeuss0910@users.noreply.github.com>
2022-07-27 21:15:42 +0530

    Add files via upload

---
+++ PRACTICE SESSION/ComparitiveOperator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+/**
+Author:Mohammed Ashwath
+Title:Comparitive Operator
+Question:Using if,else Statement
+**/
+
+public class ComparitiveOperator
+public static void main (String [] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("ENTER ANY TWO NUMBERS : ");
+
+int A= sc.nextInt(); // getting input A from user
+int B= sc.nextInt();// getting input B from user
+
+if (A!=B)//to proceed only if A and B is not equal
+{
+if (A>B)//to check whether A greater than B
+{
+System.out.println("the greater number is " +A); // if yes A> B then print this statment
+System.out.println("the lesser number is " +B); // it A is greater then next number will be lesser number
+}
+else//if A is not greater than B then else part is executed
+{
+System.out.println("the greater number is "+B);// prints B is greater
+System.out.println("the lesser number is "+A);//and A is lesser
+ }
+}
+else //this else is for the frst it condition if A and B are equal then else part will be printed directly
+{
+System.out.println("both the numbers are same"); 
+     }
+	}
+   }
