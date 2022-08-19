baf268595454c0aefb499b8ab5a99f30e1da9e2b
Zeuss0910 <107348151+Zeuss0910@users.noreply.github.com>
2022-08-19 10:59:10 +0530

    Add files via upload

---
+++ LAB SESSION/Class1.java
@@ -0,0 +1,65 @@
+public class Class1  //main class class1
+{
+public static void main(String[] args)
+{
+A ob = new A(65, 70, 75);   //an object for paramEtrized constructor of class A is created
+
+System.out.println("Student 1 Per: " +ob.getPercentage()); //println statement to get the percentage of student 1 
+
+B obj = new B(80 , 85, 90, 95);  //an object for paramEtrized constructor of class A is created
+
+
+System.out.println("Student 2 Per: " +obj.getPercentage()); //println statement to get the percentage of student 1 
+}   
+}
+
+abstract class Mark. //an abstract class mark is created 
+
+{
+abstract float getPercentage(); // abstract method for the abstract class 
+}
+
+class A extends Mark  // class A extends the anstract class mark 
+{
+float sub1 , sub2 , sub3;
+float per=0;        
+A(float a , float b, float c)// paraneterized method of class A
+{
+//where sub 1,2,3 refers to a,b,c 
+sub1 = a;
+sub2 = b;
+sub3 = c;    
+}
+@Override
+public float getPercentage() //method overriding is done 
+{
+per = (sub1 + sub2 + sub3 )/300* 100; // calculating the percentage 
+
+return per; //returning the float percentage value  as the return type is a float 
+}
+}
+
+
+class B extends Mark. // class B extends the anstract class mark 
+{
+//where sub 1,2,3 refers to a,b,c
+
+float sub1 , sub2 , sub3 , sub4;
+float per=0;        //initializing percentage variable by 0
+
+B(float a , float b, float c,float d)
+{
+sub1 = a;
+sub2 = b;
+sub3 = c;    
+sub4 = d;
+}
+@Override
+public float getPercentage() //method overriding is done 
+{
+per = (sub1 + sub2 + sub3 + sub4 )/400* 100; 
+
+return per; //returning the float percentage value  as the return type is a float 
+
+}
+}
